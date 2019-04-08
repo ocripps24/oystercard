cba0e999f44755b0e0f3acfe77f97827a7b530f0
Oliver Cripps <ollycripps@gmail.com>
2019-04-08 15:32:49 +0100

    add rspec flags, add test/method for top_up with amount

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,10 @@ describe Oystercard do
       expect(subject.balance).to eq 0
     end
   end
+
+  describe '#top_up' do
+    it "tops up the balance" do
+      expect { subject.top_up(10) }.to change{ subject.balance }.by 10
+    end
+  end
 end
